4ae9a9d5ad5e3adc9859d77dc77d2bfb9a4eddf5
Legend23 <y.laborde@rt-iut.re>
2017-03-29 22:18:08 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -11,3 +11,25 @@ print("Light: {}".format(poller.parameter_value(MI_LIGHT)))
 print("Conductivity: {}".format(poller.parameter_value(MI_CONDUCTIVITY)))
 print("Battery: {}".format(poller.parameter_value(MI_BATTERY)))
 
+#data to write into the json file
+data = {}
+data['miflora'] = []
+data['miflora'].append({
+    'Firmware': format(poller.firmware_version()),
+    'Nom': format(poller.name()),
+    'Température': format(poller.parameter_value("temperature")),
+    'Humidité': format(poller.parameter_value(MI_MOISTURE)),
+    'Lumière': format(poller.parameter_value(MI_LIGHT)),
+    'Conductivité': format(poller.parameter_value(MI_CONDUCTIVITY)),
+    'Batterie': format(poller.parameter_value(MI_BATTERY))
+})
+
+#open destination file, and use json.dump to write data
+with open('data.json','w') as outfile:
+    json.dump(data,outfile)
+
+#open destination file, and use json.load to read data
+#with open('data.json') as json_file:
+#    data = json.load(json_file)
+#    for p in data['miflora']:
+#        print(p) 
